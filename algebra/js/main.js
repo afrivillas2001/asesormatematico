6969441c8cc4eebc2f878f1953cdae98c6780bf7
Andres Rivillas <afrivilas@gmail.com>
2022-03-12 05:14:15 -0500

    estilizando_funciones 12/03/2022 5:14 a.m

--- algebra/js/main.js
+++ algebra/js/main.js
@@ -1,36 +1,37 @@
-function linear_function(operacion) {
+function linear_function(operacion = 0) {
     let md = document.getElementById("valor_m");
     let m = parseFloat(md.value);
     let bd = document.getElementById("operacion_mat");
     let b = 0 || parseFloat(bd.value);
     let x = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
     let resultado = document.getElementById("resultado");
-    for (i = 0; i in x; i++) {
-        let txt = x[i];
-        let opx = txt * m;
-        if (x.length) {
-            switch (operacion) {
-                case 1:
-                    resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx + b) + "<br>";
-                    break;
-                case 2:
-                    resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx - b) + "<br>";
-                    break;
-                case 3:
-                    resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx * b) + "<br>";
-                    break;
-                case 4:
-                    resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx / b) + "<br>";
-                    break;
-                default:
-                    resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + opx + "<br>";
-                    break;
-            }
-        }
-
-    }
     if (operacion == 5) {
 
         resultado.innerHTML = " ";
+    } else {
+        for (i = 0; i in x; i++) {
+            let txt = x[i];
+            let opx = txt * m;
+            if (x.length) {
+                switch (operacion) {
+                    case 1:
+                        resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx + b) + "<br>";
+                        break;
+                    case 2:
+                        resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx - b) + "<br>";
+                        break;
+                    case 3:
+                        resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx * b) + "<br>";
+                        break;
+                    case 4:
+                        resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + (opx / b) + "<br>";
+                        break;
+                    default:
+                        resultado.innerHTML += "Si Y vale :" + txt + " Entonces X vale: " + opx + "<br>";
+                        break;
+                }
+            }
+
+        }
     }
 }
